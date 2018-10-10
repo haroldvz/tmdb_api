029006553548464c7ac5cf2df1d896974e8e34ea
Harold Velez <haroldvelez@AL-COL-02693.local>
2018-10-10 15:38:03 -0500

    some changes in server

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const app = require('./app');
 
+app.get('/',(req,res)=>{
+    res.send('Hello');
+});
+
+app.get('/api/users/:id',(req,res)=>{
+    res.send(req.params.id);
+});
+
 const server = http.createServer(app);
 
 server.listen(port);
